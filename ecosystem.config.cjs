97e4cb3c871bd166a1becc07245636dfbd117849
Zanko <zd1179589071@163.com>
2026-04-09 17:13:17 +0800

    fix: pm2 exec_mode改为fork（cluster模式不支持--import）

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -13,6 +13,7 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 25656,
       },
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       max_memory_restart: '512M',
